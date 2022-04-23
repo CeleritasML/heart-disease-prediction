3c4e5b7c03689e8b469b156ec6acc4f237b3ce27
Rui Qiu <rq47@georgetown.edu>
2022-04-23 18:39:19 -0400

    Update xgboost model and viz

--- model-script/xgboost.R
+++ model-script/xgboost.R
@@ -4,29 +4,6 @@ pacman::p_load(tidyverse, tidymodels, janitor,
 
 font_add_google(name = "Roboto Mono", family = "Roboto Mono")
 
-# Set ggplot theme
-theme_set(theme_minimal(base_family = "Roboto Mono"))
-theme_update(
-  # panel.background = element_rect(fill = "#fbf9f4", color = "#fbf9f4"),
-  # plot.background = element_rect(fill = "#fbf9f4", color = "#fbf9f4"),
-  panel.border = element_rect(fill = NA, color = NA),
-  panel.grid.major.x = element_blank(),
-  panel.grid.major.y = element_line(linetype = "dashed"),
-  panel.grid.minor = element_blank(),
-  panel.grid = element_line(color = "#b4aea9"),
-  axis.text.x = element_text(size = 10),
-  axis.text.y = element_text(size = 10),
-  axis.title.x = element_text(size = 13, colour = "#495057", face = "bold"),
-  axis.title.y = element_text(size = 13, colour = "#495057",  margin = margin(r = 10), face = "bold"),
-  axis.line = element_line(colour = "grey50"),
-  legend.title = element_text(size = 13, face = "bold", colour = "#495057"),
-  legend.text = element_text(size = 10, color = "#495057"), 
-  plot.title = element_text(hjust = 0.5, size = 15, face = "bold", color = "#2a475e"),
-  plot.caption = element_text(family = "Special Elite", size = 10, color = "grey70", face = "bold",
-                              hjust = .5, margin = margin(5, 0, 20, 0)),
-  plot.margin = margin(10, 25, 10, 25),
-)
-
 dat <- read_csv("data/heart_2020_cleaned.csv") |>
   clean_names()
 
@@ -78,7 +55,7 @@ heart_folds <- vfold_cv(heart_train, v = 10, strata = heart_disease)
 #   theme_minimal()
 
 heart_recipe <- recipe(heart_disease ~ ., data = heart_train) |>
-  step_dummy(all_nominal_predictors(), one_hot=TRUE) |>
+  step_dummy(all_nominal_predictors(), one_hot=FALSE) |>
   step_zv(all_predictors()) # precautionary step to remove variables only containing a single value
 
 prep(heart_recipe)
@@ -174,6 +151,29 @@ collect_predictions(stopping_fit) |>
 
 save.image("data/xgboost_w_early_stopping.RData")
 load("data/xgboost_w_early_stopping.RData")
+
+# Set ggplot theme
+theme_set(theme_minimal(base_family = "Roboto Mono"))
+theme_update(
+  # panel.background = element_rect(fill = "#fbf9f4", color = "#fbf9f4"),
+  # plot.background = element_rect(fill = "#fbf9f4", color = "#fbf9f4"),
+  panel.border = element_rect(fill = NA, color = NA),
+  panel.grid.major.x = element_blank(),
+  panel.grid.major.y = element_line(linetype = "dashed"),
+  panel.grid.minor = element_blank(),
+  panel.grid = element_line(color = "#b4aea9"),
+  axis.text.x = element_text(size = 10),
+  axis.text.y = element_text(size = 10),
+  axis.title.x = element_text(size = 13, colour = "#495057", face = "bold"),
+  axis.title.y = element_text(size = 13, colour = "#495057",  margin = margin(r = 10), face = "bold"),
+  axis.line = element_line(colour = "grey50"),
+  legend.title = element_text(size = 13, face = "bold", colour = "#495057"),
+  legend.text = element_text(size = 10, color = "#495057"), 
+  plot.title = element_text(hjust = 0.5, size = 15, face = "bold", color = "#2a475e"),
+  plot.caption = element_text(family = "Special Elite", size = 10, color = "grey70", face = "bold",
+                              hjust = .5, margin = margin(5, 0, 20, 0)),
+  plot.margin = margin(10, 25, 10, 25),
+)
 
 extract_workflow(stopping_fit) |>
   extract_fit_parsnip() |>
